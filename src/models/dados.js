0d4df5cb383c0814649b701122318dd338aab8e3
victor <victorfoliveira0407@gmail.com>
2025-09-22 13:31:56 -0300

    Servidor funcionando

---
+++ src/models/dados.js
@@ -0,0 +1,114 @@
+const camisetas = [
+    {
+      "id": 1,
+      "design": "Código Binário",
+      "tema": "Programação",
+      "tamanho": "M",
+      "marca": "GeekWear",
+      "preco": 59.90,
+      "material": "Algodão 100%",
+      "categoria": "TI",
+      "estoque": 25
+    },
+    {
+      "id": 2,
+      "design": "Lord of the Rings - One Ring",
+      "tema": "Fantasia",
+      "tamanho": "G",
+      "marca": "FantasyTees",
+      "preco": 69.90,
+      "material": "Poliéster",
+      "categoria": "Cinema",
+      "estoque": 12
+    },
+    {
+      "id": 3,
+      "design": "Star Wars - Darth Vader",
+      "tema": "Ficção Científica",
+      "tamanho": "GG",
+      "marca": "SciFiThreads",
+      "preco": 75.00,
+      "material": "Algodão Penteado",
+      "categoria": "Cinema",
+      "estoque": 8
+    },
+    {
+      "id": 4,
+      "design": "Pokeball",
+      "tema": "Animes e Mangás",
+      "tamanho": "P",
+      "marca": "AnimeStyle",
+      "preco": 49.90,
+      "material": "Malha Fria",
+      "categoria": "Animes",
+      "estoque": 30
+    },
+    {
+      "id": 5,
+      "design": "Super Mario - Cogumelo",
+      "tema": "Games",
+      "tamanho": "M",
+      "marca": "GameOn",
+      "preco": 55.00,
+      "material": "Algodão 100%",
+      "categoria": "Jogos",
+      "estoque": 18
+    },
+    {
+      "id": 6,
+      "design": "The Matrix - Neo",
+      "tema": "Ficção Científica",
+      "tamanho": "G",
+      "marca": "GeekWear",
+      "preco": 65.00,
+      "material": "Poliéster",
+      "categoria": "Cinema",
+      "estoque": 15
+    },
+    {
+      "id": 7,
+      "design": "Astro - Robô Futurista",
+      "tema": "Robótica",
+      "tamanho": "GG",
+      "marca": "TechThreads",
+      "preco": 62.50,
+      "material": "Dry Fit",
+      "categoria": "Tecnologia",
+      "estoque": 7
+    },
+    {
+      "id": 8,
+      "design": "Dragon Ball Z - Goku",
+      "tema": "Animes e Mangás",
+      "tamanho": "M",
+      "marca": "AnimeStyle",
+      "preco": 70.00,
+      "material": "Algodão Penteado",
+      "categoria": "Animes",
+      "estoque": 22
+    },
+    {
+      "id": 9,
+      "design": "Gamer Since - 1980",
+      "tema": "Games",
+      "tamanho": "P",
+      "marca": "GameOn",
+      "preco": 59.90,
+      "material": "Algodão 100%",
+      "categoria": "Jogos",
+      "estoque": 10
+    },
+    {
+      "id": 10,
+      "design": "Cthulhu",
+      "tema": "Terror e Fantasia",
+      "tamanho": "G",
+      "marca": "FantasyTees",
+      "preco": 79.90,
+      "material": "Poliéster",
+      "categoria": "Literatura",
+      "estoque": 5
+    }
+  ];
+
+  export default {camisetas}
